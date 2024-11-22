d6b86bfb2bd4cf1b4d7f19ead783e6a5747c14f9
MS <msteenbock457@gmail.com>
2024-11-21 22:02:40 -0700

    Mobile Viewport: Index and Template

---
+++ script/script.js
@@ -0,0 +1,18 @@
+/*
+*
+*
+*
+* */
+
+
+
+//Hamburger menu function, toggles the mobile viewport's navigation bar.
+function hamburgerMenu() {
+    var navlinks = document.getElementById("nav-links-mobile");
+    var menuicon = document.getElementById("hamburger-icon");
+    if (navlinks.style.display === "block") {
+        navlinks.style.display = "none";
+    } else {
+        navlinks.style.display = "block";
+    }
+}
