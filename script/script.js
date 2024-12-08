a78aa472ad02e6309f1732cc8a51a6c6d073ec5b
MS <msteenbock457@gmail.com>
2024-12-07 22:15:26 -0700

    Articles content filled in, most citations filled in, gallery content populated, quiz made for article three, style for quiz as well.

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@
 */
 
 
+var quizAnswer = document.querySelector("#quiz-answer p");
+var answerHeader = document.querySelector("#quiz-answer h3");
 
 //Hamburger menu function, toggles the mobile viewport's navigation bar.
 function hamburgerMenu() {
@@ -13,4 +15,25 @@ function hamburgerMenu() {
     } else {
         navlinks.style.display = "block";
     }
+}
+
+//Functions that shows the user the answers to the quiz.
+function answer1() {
+    answerHeader.style.display = "block";
+    quizAnswer.textContent = "true. Geologic maps can help us discover all sorts of resources we depend on.";
+}
+
+function answer2() {
+    answerHeader.style.display = "block";
+    quizAnswer.textContent = "false. Geologists still need to conduct fieldwork, it is just made more efficient with these tools.";
+}
+
+function answer3() {
+    answerHeader.style.display = "block";
+    quizAnswer.textContent = "false. Geologic maps can be used for so much more than identifying rock ages and types.";
+}
+
+function answer4() {
+    answerHeader.style.display = "block";
+    quizAnswer.textContent = "true. Without data driven technology geologists would struggle to make quick and efficient decisions.";
 }
