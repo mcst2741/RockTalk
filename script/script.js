fa02f3e0f38828450fb97d743c024159b1216e8a
MS <msteenbock457@gmail.com>
2024-12-08 20:17:05 -0700

    Style fixes, added a few things missed in the rubric.Credits populated and images too.

--- script/script.js
+++ script/script.js
@@ -1,39 +1,50 @@
 /*
-* Mckenna Steenbock
+* Author: Mckenna Steenbock
 */
 
 
-var quizAnswer = document.querySelector("#quiz-answer p");
-var answerHeader = document.querySelector("#quiz-answer h3");
+const quizAnswer = document.querySelector("#quiz-answer p");
+const answerHeader = document.querySelector("#quiz-answer h3");
+const audioClip = document.getElementById("audio-clip");
 
 //Hamburger menu function, toggles the mobile viewport's navigation bar.
 function hamburgerMenu() {
-    var navlinks = document.getElementById("nav-links-mobile");
-    var menuicon = document.getElementById("hamburger-icon");
-    if (navlinks.style.display === "block") {
-        navlinks.style.display = "none";
+    const navLinks = document.getElementById("nav-links-mobile");
+    const menuIcon = document.getElementById("hamburger-icon");
+    if (navLinks.style.display === "block") {
+        navLinks.style.display = "none";
     } else {
-        navlinks.style.display = "block";
+        navLinks.style.display = "block";
     }
+}
+
+//Function to play pebble audio clip when quiz answers are clicked of the form buttons have been clicked.
+function playAudioClip (){
+    audioClip.currentTime = 0;
+    audioClip.play();
 }
 
 //Functions that shows the user the answers to the quiz.
 function answer1() {
     answerHeader.style.display = "block";
+    playAudioClip();
     quizAnswer.textContent = "true. Geologic maps can help us discover all sorts of resources we depend on.";
 }
 
 function answer2() {
     answerHeader.style.display = "block";
+    playAudioClip();
     quizAnswer.textContent = "false. Geologists still need to conduct fieldwork, it is just made more efficient with these tools.";
 }
 
 function answer3() {
     answerHeader.style.display = "block";
+    playAudioClip();
     quizAnswer.textContent = "false. Geologic maps can be used for so much more than identifying rock ages and types.";
 }
 
 function answer4() {
     answerHeader.style.display = "block";
+    playAudioClip();
     quizAnswer.textContent = "true. Without data driven technology geologists would struggle to make quick and efficient decisions.";
 }
